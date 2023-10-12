17c7572d137fe43040fb87f2000b330291b46a14
Kate Hooker <katehooker88@gmail.com>
2023-10-12 08:29:44 +0000

    plus minus add and delet added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if (payload.digit === '0' && state.currentOperand === '0') {
         return state
       }
@@ -24,9 +31,96 @@ function reducer(state, { type, payload }) {
         ...state,
         currentOperand: `${state.currentOperand || ''}${payload.digit}`,
       }
+
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        }
+      }
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null,
+        }
+      }
+
+      return {
+        ...state,
+        surrentOperand: state.currentOperand.slice(0, -1),
+      }
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state
+      }
+
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operantion: payload.operation,
+        }
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.cureentOperand,
+          currentOperand: null,
+        }
+      }
+
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        opeartion: payload.operation,
+        currentOperand: null,
+      }
+
     case ACTIONS.CLEAR:
       return {}
+
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation == null ||
+        state.operand == null ||
+        state.previousOperand == null
+      ) {
+        return state
+      }
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      }
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN(current)) return ''
+  let computation = ''
+  switch (operation) {
+    case '+':
+      computation = prev + current
+      break
+    case '-':
+      computation = prev - current
+      break
+    case '%':
+      computation = prev / current
+      break
+    case '*':
+      computation = prev * current
+      break
+  }
+  return computation.toString()
 }
 
 function App() {
@@ -49,7 +143,9 @@ function App() {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELET_DIGIT })}>
+        DEL
+      </button>
       <OperationButton operation="%" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -67,7 +163,12 @@ function App() {
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
 
-      <button clasName="span-two">=</button>
+      <button
+        clasName="span-two"
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+      >
+        =
+      </button>
     </div>
   )
 }
